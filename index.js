816d6c6c5d78e8d3e8751bcb7074ecb19708c96e
BenardAbwao <benard.okoth.abwao@gmail.com>
2022-06-24 00:54:02 +0300

    add a review function

--- index.js
+++ index.js
@@ -204,7 +204,18 @@ let products = {
       }
     });
   });
-
+  productsReviewForm.addEventListener('submit', (env) => {
+    env.preventDefault();
+    console.log(`review form ID: ${products.id}`)
+    console.log('---------------------------------')
+    if(productsReviewText.value !== ''){
+        
+        products.reviews.push(productsReviewText.value)
+        patchBeer(products)
+    } else{
+        alert('Review is empty string!!')
+    }
+});
   window.onload = () => {
     filterProduct("all");
   };
